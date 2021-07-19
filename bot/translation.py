f6fd72cb352ddea10c4c6a0c57c1ea86928c5714
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-07-19 07:55:46 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+you can't add Me To 🤭🤭🤭
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@newmoviesottrelease
 </i>"""    
     
     HELP_TEXT = """
